ccc1c5085cd005fa6baa0e412922b3fb8754910c
Denzyl Dick <2477646+denzyldick@users.noreply.github.com>
2025-04-16 20:47:34 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,7 @@
       <RouterView></RouterView>
     </v-main>
   </v-app>
-
-  <!--    <v-row justify="center">-->
-  <!--      <v-dialog-->
-  <!--          v-model="dialog"-->
-  <!--          max-width="400"-->
-  <!--      >-->
-  <!--        <v-card-->
-  <!--            :loading="loading"-->
-  <!--        >-->
-  <!--          <template slot="progress">-->
-  <!--            <v-progress-linear-->
-  <!--                color="deep-purple"-->
-  <!--                height="10"-->
-  <!--                indeterminate-->
-  <!--            ></v-progress-linear>-->
-  <!--          </template>-->
-
-  <!--          <v-img-->
-  <!--              src="/img/logo.svg"-->
-  <!--          ></v-img>-->
-  <!--          <v-card-text>-->
-  <!--            <v-row>-->
-  <!--              <v-col>-->
-  <!--              </v-col>-->
-  <!--            </v-row>-->
-  <!--          </v-card-text>-->
-  <!--          <v-card-text>-->
-  <!--          </v-card-text>-->
-  <!--        </v-card>-->
-  <!--      </v-dialog>-->
-
-  <!--    </v-row>-->
 </template>
-
 <script>
 import Home from "./views/Home.vue";
 import Setting from "./views/Setting.vue";
